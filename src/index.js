b859ac383bb00104e686b8ed24d521e53904b628
Tristan Rollet <tristan.rollet38@gmail.com>
2023-12-21 16:45:01 +0100

    Add QuizzCord documentation link and typedef

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const Quizz = require('./functions/index.js');
 
+/**
+ * # QuizzCord
+ * Check out the [documentation](https://www.npmjs.com/package/quizzcord) for more information.
+ * 
+ * @typedef {Object} Exports
+ * @property {typeof Quizz} Quizz
+ */
+
+/** @type {Exports} */
 module.exports = {
 	Quizz
 };
